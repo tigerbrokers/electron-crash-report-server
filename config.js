59b3a9a20178ef2b0293520c63ec2ebb16683a37
john muhl <git@johnmuhl.com>
2015-09-04 16:03:35 -0500

    :art: Move config to module

---
+++ config.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const fs = require('fs')
+const ini = require('ini')
+const config = ini.parse(fs.readFileSync('config.ini', 'utf-8'))
+
+module.exports = config
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict'
 
+const config = require('./config')
 const express = require('express')
 const fs = require('fs')
-const ini = require('ini')
 const saveCrashReport = require('./save-crash-report')
 
 fs.accessSync('.', fs.W_OK)
 fs.accessSync('config.ini', fs.R_OK)
 
 const app = express()
-const config = ini.parse(fs.readFileSync('config.ini', 'utf-8'))
 const server = app.listen(config.port, function () {
   const addr = server.address().address
   const port = server.address().port
