282fd8ed482b3792fc139d27c07bbfc5ee4a6ba3
john muhl <git@johnmuhl.com>
2016-12-15 20:43:14 -0600

    💣 Add an exploding electron application

---
+++ electron-bomb/index.js
@@ -0,0 +1,42 @@
+const {
+  app,
+  BrowserWindow,
+  crashReporter
+} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+crashReporter.start({
+  companyName: 'Company Name',
+  submitURL: 'http://localhost:3000/',
+  autoSubmit: true,
+  ignoreSystemCrashHandler: true,
+  extra: {'extra': 'info from the main process'}
+})
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file'
+  }))
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (win === null) createWindow()
+})
