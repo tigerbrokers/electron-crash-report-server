aacd1d8201321022a224e03ca4e68514ad65d4f9
john muhl <git@johnmuhl.com>
2016-12-17 13:33:36 -0600

    🔥

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const fs = require('fs')
-const ini = require('ini')
-const path = require('path')
-const config = ini.parse(fs.readFileSync(path.join(__dirname, 'config.ini'), 'utf-8'))
-
-module.exports = config
